bf52ece2c01ea6d170514376ef45cb2db1d85ba2
Miguel-klk <124157850+Miguel-klk@users.noreply.github.com>
2023-04-19 12:59:31 +0200

    Actividad 5

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt("Primer Número:"))
+let num2 = parseFloat(prompt("Segundo Número"))
+if (num1 > num2) {alert("El número" + num1 + "es mayor que el número" + num2)}
+else if (num1 < num2) {alert("El número " + num1 + "es menor que el número " + num2)}
+else if (num1 == num2) {alert("El número " + num1 + "y el número " + num2 + "son números iguales ")}
+if (num2 % 2 == 0) {alert("El número " + num2 + "es un número par")}
+else {alert("El número " + num2 + "es un número impar")}
